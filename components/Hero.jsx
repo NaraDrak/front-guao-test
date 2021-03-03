2c87e8d297dc7eafe33fc326fdb5f7587fb5c743
Guillermo Villarreal Gallegos <memowars@outlook.es>
2021-03-02 19:08:18 -0600

    sm:border fixed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,11 +4,10 @@ import RightDraw from "./InfoContactoCard";
 export default function Hero() {
   return (
     <div className="bg-contain bg-no-repeat bg-hero-pattern">
-      <div className="flex flex-col md:flex-row lg:flex-row justify-between">
+      <div className="flex flex-col md:flex-row lg:flex-row lg:pr-0 pr-16">
         <LeftDraw />
-        <div className="flex justify-center">
-          <RightDraw />
-        </div>
+
+        <RightDraw />
       </div>
     </div>
   );
